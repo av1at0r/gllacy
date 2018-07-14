5ea72bba41b2ccded2fac50049fdd3c70decdd77
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-14 17:32:31 +0000

    Deploy av1at0r/gllacy to github.com/av1at0r/gllacy.git:gh-pages

--- js/index.js
+++ js/index.js
@@ -42,3 +42,24 @@ indexFeedbackCloseButton.onclick = function() {
   document.getElementById('index-feedback').classList.remove('feedback--active');
   indexFeedbackShowButton.focus();
 }
+
+//Карта
+
+window.onload = function() {
+  var map = new ymaps.Map('map', {
+    center: [59.9387, 30.3247],
+    zoom: 18
+  });
+  var placemark = new ymaps.Placemark([59.93863, 30.32305], {
+      hintContent: 'Большая Конюшенная 19/8, Санкт-Петербург',
+      balloonContent: 'Gllacy - Магазин мороженного'
+    },
+    {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/pin.png',
+      iconImageSize: [80, 140],
+      iconImageOffset: [-40, -140]
+  });
+
+  map.geoObjects.add(placemark);
+}
